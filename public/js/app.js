28b5e79dec75f27a79bb183c392c2c32fd32ef04
PFletcher <PFletcher>
2016-06-28 18:36:15 -0400

    completed grid layout

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,7 @@ initGrid();
   function initGrid(){
     for(var i = 0; i< numOfRows; i += 1){
       var row = $('<div></div>');
+      row.addClass('row');
       for(var j = 0; j < numOfCols; j += 1){
         var cell = $('<div></div>');
         cell.addClass('cell border');
